f030f6a3bd826c327e62b9b4568608ee3c886ed9
Janine Cunha <janinecunha213@gmail.com>
2026-08-08 17:49:54 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.reconhecimentolocal.web',
+  appName: 'Reconhecimento Local Web',
+  webDir: 'www'
+};
+
+export default config;
